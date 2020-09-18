b81d8e981ab86053bce9cb7d09e0d7c0c85b4a62
Tyler Heald <tyler@waveofdisdain.com>
2020-09-18 11:11:18 -0500

    Added basic movement on the x,z plane
    The character can only move on the x,z plane and only on fixed tracks. Left is always the direction it is at the start, and the same is true for the rest of the directions.

---
+++ Assets/PlayerControlScript.cs
@@ -0,0 +1,36 @@
+﻿/* Programmers: Tyler Heald
+ * Date: 9/18/2020
+ * Class: CS 6334
+ * Purpose: 
+ * 9/18/20 - Give the player simple movement with the arrow keys
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlScript : MonoBehaviour
+{
+    //VARIABLES
+    private Vector3 playerMove;
+    private float playerSpeed = 2.0f;
+    private CharacterController characterController;
+    private Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Getting the controller in order to provide movement to it
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get input for the player move
+        playerMove = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        //Moving the character with the delta so it is based on how much time passed between frames
+        characterController.Move(playerMove * Time.deltaTime * playerSpeed);
+    }
+}
